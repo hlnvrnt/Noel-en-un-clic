a949fdb7825e5b67f010d3e9e79ca69ea1fcb5cf
FUKUI AKANE <akane.2702@gmail.com>
2023-11-23 17:58:25 +0100

    commit after pull

--- noel-en-un-clic/src/pages/Menu.jsx
+++ noel-en-un-clic/src/pages/Menu.jsx
@@ -1,28 +1,34 @@
 import { NavLink } from 'react-router-dom';
-
-
 function Menu(){
 
     return(
         <>
-        <NavLink to="/">
-        <div className="renne">
-        <img src="/images/reindeer.png" alt="rennes" />
-        </div>
-        </NavLink>
-        <div className="menu-container">
-            <div className="lutin-container">
-                <img src="../../public/page_menu/before.png" className="menu-card-icon" alt="icon-before" />
-                <p>Miam !Qu’il y aura t’il au menu ?</p>
-
+        <body>
+            <NavLink to="/">
+            <div className="renne">
+            <img src="/images/reindeer.png" alt="rennes" />
             </div>
-            <div className="menu-card-container">
-                <h1>Recettes de noël</h1>
-                <img src="../../public/page_menu/before.png" className="menu-card-icon" alt="icon-before" />
-                <div className="repas-card-container">
-                    <div className="repas-category">
-                        <img src="../../public/page_menu/dinde.jpeg" className="menu-card-icon" alt="recette-image" />
-                        <p>Plat</p>
+            </NavLink>
+            <div className="menu-container">
+                <div className="menu-card-container">
+                    <h1>Recettes de noël</h1>
+                    <div className="arrow-repas-container">
+                        <div className="arrow-container">
+                            <img src="../../public/page_menu/before.png" className="arrow" alt="icon-before" />
+                        </div>
+                        <div className="repas-card-container">
+                            <div className="repas-image-category-container">
+                                <img src="../../public/page_menu/dinde.jpeg" className="menu-card-icon" alt="recette-image" />
+                                    <p>Plat</p>
+                            </div>
+                            <div className="repasname-button-container">
+                                <h2>Nom de repas</h2>
+                                <button>Ajouter au menu</button>
+                            </div>
+                        </div>
+                        <div className="arrow-container">
+                            <img src="../../public/page_menu/next.png" className="arrow" alt="icon-next" />
+                        </div>
                     </div>
                 </div>
                 <div className="chosen-menu-container">
@@ -34,15 +40,15 @@ function Menu(){
                     </div>
                     <div className="menu-name-container">
                         <div className="left-block">
-                            <div className="couvert-icon-container">
-                                <img src="../../public/page_menu/couvertatable.png" className="menu-card-icon" alt="menu-couvert" />
-                            </div>
-                            <div className="menu-category">
-                                Plat
-                            </div>
-                            <div className="chosen-menu-name">
-                                <p>Dinde de Noel</p>
-                            </div>
+                        <div className="couvert-icon-container">
+                            <img src="../../public/page_menu/couvertatable.png" className="menu-card-icon" alt="menu-couvert" />
+                        </div>
+                        <div className="menu-category">
+                            Plat
+                        </div>
+                        <div className="chosen-menu-name">
+                            <p>Dinde de Noel</p>
+                        </div>
                         </div>
                         <div className="delete-icon-container">
                             <img src="../../public/page_menu/delete.png" className="menu-card-icon" alt="icon-delete" />
